7e88db16390c674e670175a37450c33e270589de
dokeys <dokeys28@gmail.com>
2023-08-23 12:13:03 -0400

    animacion hecha

--- imagen.py
+++ imagen.py
@@ -4,14 +4,17 @@ import pygame
 class Imagen:
     def __init__(self, imagen):
         self.imagen = imagen
-        self.numero_imagen = 0
         #define cada cuantos frames se refresca la imagen
         self.frame_rate = 5
         #secuencia, variacion
+        self.frame = 0
+        self.ancho_de_sprites = 64
+        self.alto_de_sprites = 64
+        self.numero_imagen = 0
+        self.numero_variacion = 0
+        self.tamano_de_sprites = (self.ancho_de_sprites, self.alto_de_sprites)
         self.cantidad_de_imagenes = self.obtener_cantidad_de_imagenes()
         self.secuencia_de_imagenes, self.variacion_de_imagenes = self.cantidad_de_imagenes
-        self.frame = 0
-        self.tamano_de_sprites = (64,64)
     
     
     def obtener_cantidad_de_imagenes(self) -> tuple:
@@ -21,10 +24,22 @@ class Imagen:
         return filas, columnas
     
         
-    def animacion(self):
-        self.imagen_actual = self.imagen.subsurface(pygame.Rect(self.numero_imagen,128,64,64))
-        if self.numero_imagen < self.secuencia_de_imagenes:
+    def animacion(self)-> pygame.image:
+        self.pixel_numero_imagen = self.numero_imagen * self.ancho_de_sprites
+        self.pixel_variacion_de_imagenes = self.numero_variacion * self.alto_de_sprites
+        self.imagen_actual = self.imagen.subsurface(pygame.Rect(self.pixel_numero_imagen,
+                                                                self.pixel_variacion_de_imagenes, 
+                                                                self.ancho_de_sprites, 
+                                                                self.alto_de_sprites))
+        if self.numero_imagen < self.secuencia_de_imagenes -1 :
             if self.frame == self.frame_rate:
-                self.numero_imagen += 64
+                self.numero_imagen += 1
+        else:
+            self.numero_imagen = 0
+            
         if self.frame < self.frame_rate:
             self.frame += 1
+        else:
+            self.frame = 0
+        
+        return self.imagen_actual
--- prueba.py
+++ prueba.py
@@ -1,13 +1,15 @@
 import pygame
 
+from imagen import Imagen
+
 
 
 pygame.init()
 pantalla = pygame.display.set_mode((600,600))
 pygame.display.set_caption('PRUEBAS RPG')
 imagen = pygame.image.load('./imagenes/personaje/caminar.png')
-n = 0
-f = 0
+imagen = Imagen(imagen)
+imagen.numero_variacion = 2
 while True:
     
     pantalla.fill((255,255,255))
@@ -15,17 +17,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    if n < 512 and f == 5:
-        n += 64
-    if n >= 512:
-        n = 0
-    if f < 5:
-        f += 1
-    else:
-        f = 0
-    
-    imagen_recortada = imagen.subsurface(pygame.Rect(n,128,64,64))
-    pantalla.blit(imagen_recortada,(0,0))
 
+    
+
+    pantalla.blit(imagen.animacion(),(0,0))
+        
     pygame.display.flip()
     pygame.time.Clock().tick(60)
