547735fdec90364b5ffac58ffbf9d0b96d10cc77
dokeys <dokeys28@gmail.com>
2023-08-23 12:16:35 -0400

    teleport

--- prueba.py
+++ prueba.py
@@ -1,15 +1,12 @@
 import pygame
 
-from imagen import Imagen
 
 
 
 pygame.init()
 pantalla = pygame.display.set_mode((600,600))
 pygame.display.set_caption('PRUEBAS RPG')
-imagen = pygame.image.load('./imagenes/personaje/caminar.png')
-imagen = Imagen(imagen)
-imagen.numero_variacion = 2
+
 while True:
     
     pantalla.fill((255,255,255))
@@ -20,7 +17,7 @@ while True:
 
     
 
-    pantalla.blit(imagen.animacion(),(0,0))
+    #pantalla.blit(imagen.animacion(),(0,0))
         
     pygame.display.flip()
     pygame.time.Clock().tick(60)
