0fc5b551bb7ec30d1dee6f1951c25a84932841e2
dokeys <dokeys28@gmail.com>
2023-09-12 12:30:36 -0400

    Sistema de Niveles y experiencia

--- stats.py
+++ stats.py
@@ -44,21 +44,32 @@ class Stat:
     def subir(self):
         self.nivel += 1
 
+
+class TotalLevel(Stat):
+    def __init__(self, nombre, stats):
+        self.stats = stats
+        super().__init__(nombre)
+        
+    def total(self):
+        x = sum([s.nivel for s in self.stats])
+        self.nivel = x//len(self.stats)
+             
+        
         
 class Stats:
     def __init__(self, juego):
         self.juego = juego
-        self.level = Stat('Level')
         self.ataque = Stat('Ataque')
         self.defensa = Stat('Defensa')
         self.arqueria = Stat('Arqueria')
         self.magia = Stat('Magia')
+        self.stats = [self.ataque, self.defensa, self.arqueria, self.magia]
+        self.level = TotalLevel('Level',self.stats)
         self.visible = False
-        self.stats = [self.level, self.ataque, self.defensa, self.arqueria, self.magia]
     
     def mostrar_stats(self):
         for i, s in enumerate(self.stats):
             s.exp.actualizar()
             self.juego.pantalla.blit(FUENTES.STATS.value.render(f'{s.nombre}: {s.nivel}  Exp:{s.exp.valor}//{s.exp.exp_siguiente_nivel}//{s.exp.exp_restante}',True,(0,0,0)), (0, i * 32))
-
+            self.juego.pantalla.blit(FUENTES.STATS.value.render(f'{self.level.nombre}: {self.level.nivel}',True,(0,0,0)), (0, (len(self.stats) + 1)* 32))
             
